1e3b00fe729f7b1dbc9a128a24d79f0d836b9ce6
Snayder187 <33837035+Snayder187@users.noreply.github.com>
2025-05-19 08:51:25 -0500

    Guardando Backend

---
+++ Backend/PeliculasAPI/PeliculasAPI/Controllers/PeliculasController.cs
@@ -0,0 +1,174 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using PeliculasAPI.Servicios;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/peliculas")]
+    [ApiController]
+    public class PeliculasController: CustomBaseController
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IOutputCacheStore outputCacheStore;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private const string cacheTag = "peliculas";
+        private readonly string contenedor = "peliculas";
+
+        public PeliculasController(ApplicationDbContext context, IMapper mapper,
+            IOutputCacheStore outputCacheStore, IAlmacenadorArchivos almacenadorArchivos)
+            :base(context, mapper, outputCacheStore, cacheTag)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.outputCacheStore = outputCacheStore;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpGet("landing")]
+        [OutputCache(Tags = [cacheTag])]
+        public async Task<ActionResult<LandingPageDTO>> Get()
+        {
+            var top = 6;
+            var hoy = DateTime.Today;
+
+            var proximosEstrenos = await context.Peliculas
+                .Where(p => p.FechaLanzamiento > hoy)
+                .OrderBy(p => p.FechaLanzamiento)
+                .Take(top)
+                .ProjectTo<PeliculaDTO>(mapper.ConfigurationProvider)
+                .ToListAsync();
+            
+            var enCines = await context.Peliculas
+                .Where(p => p.PeliculasCines.Select(pc => pc.PeliculaId).Contains(p.Id))
+                .OrderBy(p => p.FechaLanzamiento)
+                .Take(top)
+                .ProjectTo<PeliculaDTO>(mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            var resultado = new LandingPageDTO();
+            resultado.EnCines = enCines;
+            resultado.ProximosEstrenos = proximosEstrenos;
+            return resultado;
+        }
+
+        [HttpGet("{id:int}", Name = "ObtenerPeliculaPorId")]
+        [OutputCache(Tags = [cacheTag])]
+        public async Task<ActionResult<PeliculaDetallesDTO>> Get(int id)
+        {
+            var pelicula = await context.Peliculas
+                .ProjectTo<PeliculaDetallesDTO>(mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (pelicula is null)
+            {
+                return NotFound();
+            }
+            return pelicula;
+        }
+
+        [HttpGet("PostGet")]
+        public async Task<ActionResult<PeliculasPostGetDTO>> PostGet()
+        {
+            var cines = await context.Cines.ProjectTo<CineDTO>(mapper.ConfigurationProvider).ToListAsync();
+            var generos = await context.Generos.ProjectTo<GeneroDTO>(mapper.ConfigurationProvider).ToListAsync();
+            return new PeliculasPostGetDTO
+            {
+                Cines = cines,
+                Generos = generos
+            };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] PeliculaCreacionDTO peliculasCreacionDTO)
+        {
+            var pelicula = mapper.Map<Pelicula>(peliculasCreacionDTO);
+            if (peliculasCreacionDTO.Poster is not null)
+            {
+                var url = await almacenadorArchivos.Almacenar(contenedor, peliculasCreacionDTO.Poster);
+                pelicula.Poster = url;
+            }
+
+            AsignarOrdenActores(pelicula);
+            context.Add(pelicula);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            var peliculaDTO = mapper.Map<PeliculaDTO>(pelicula);
+            return CreatedAtRoute("ObtenerPeliculaPorId", new { id = pelicula.Id }, peliculaDTO);
+        }
+
+        [HttpGet("PutGet/{id:int}")]
+        public async Task<ActionResult<PeliculasPutGetDTO>> PutGet(int id)
+        {
+            var pelicula = await context.Peliculas
+                                .ProjectTo<PeliculaDetallesDTO>(mapper.ConfigurationProvider)
+                                .FirstOrDefaultAsync(x => x.Id == id);
+            if (pelicula is null)
+            {
+                return NotFound();
+            }
+
+            var generosSeleccionadosIds = pelicula.Generos.Select(g => g.Id).ToList();
+            var generosNoSeleccionados = await context.Generos
+                                                .Where(g => !generosSeleccionadosIds.Contains(g.Id))
+                                                .ProjectTo<GeneroDTO>(mapper.ConfigurationProvider)
+                                                .ToListAsync();
+
+            var cinesSeleccionadosIds = pelicula.Cines.Select(c => c.Id).ToList();
+            var cinesNoSeleccionados = await context.Cines
+                                            .Where(c => !cinesSeleccionadosIds.Contains(c.Id))
+                                            .ProjectTo<CineDTO>(mapper.ConfigurationProvider)
+                                            .ToListAsync();
+
+            var respuesta = new PeliculasPutGetDTO();
+            respuesta.Pelicula = pelicula;
+            respuesta.GenerosSeleccionados = pelicula.Generos;
+            respuesta.GenerosNoSeleccionados = generosNoSeleccionados;
+            respuesta.CinesSeleccionados = pelicula.Cines;
+            respuesta.CinesNoSeleccionados = cinesNoSeleccionados;
+            respuesta.Actores = pelicula.Actores;
+            return respuesta;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, [FromForm] PeliculaCreacionDTO peliculaCreacionDTO)
+        {
+            var pelicula = await context.Peliculas
+                                .Include(p => p.PeliculasActores)
+                                .Include(p => p.PeliculasCines)
+                                .Include(p => p.PeliculasGeneros)
+                                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pelicula is null)
+            {
+                return NotFound();
+            }
+
+            pelicula = mapper.Map(peliculaCreacionDTO, pelicula);
+            if (peliculaCreacionDTO.Poster is not null)
+            {
+                pelicula.Poster = await almacenadorArchivos.Editar(pelicula.Poster,
+                    contenedor, peliculaCreacionDTO.Poster);
+            }
+            AsignarOrdenActores(pelicula);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return NoContent();
+        }
+
+        private void AsignarOrdenActores(Pelicula pelicula)
+        {
+            if (pelicula.PeliculasActores is not null)
+            {
+                for (int i = 0; i < pelicula.PeliculasActores.Count; i++)
+                {
+                    pelicula.PeliculasActores[i].Orden = i;
+                }
+            }
+        }
+    }
+}
---
+++ Backend/PeliculasAPI/PeliculasAPI/Program.cs
@@ -0,0 +1,70 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using PeliculasAPI;
+using PeliculasAPI.Servicios;
+using PeliculasAPI.Utilidades;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(proveedor => new MapperConfiguration(configuracion =>
+{
+    var geometryFactory = proveedor.GetRequiredService<GeometryFactory>();
+    configuracion.AddProfile(new AutoMapperProfiles(geometryFactory));
+}).CreateMapper());
+
+builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
+    opciones.UseSqlServer("name=DefaultConnection", sqlServer => 
+    sqlServer.UseNetTopologySuite()));
+
+builder.Services.AddSingleton<GeometryFactory>(NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326));
+
+builder.Services.AddOutputCache(opciones =>
+{
+    opciones.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(60);
+});
+
+var origenesPermitidos = builder.Configuration.GetValue<string>("origenesPermitidos")!.Split(",");
+
+//Configuración CORS - Solo Navegadores
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddDefaultPolicy(opcionesCORS =>
+    {
+        opcionesCORS.WithOrigins(origenesPermitidos).AllowAnyMethod().AllowAnyHeader().
+            WithExposedHeaders("cantidad-total-registros"); //Esto es para pruebas
+    });
+});
+
+//builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosAzure>();
+builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCors();
+
+app.UseOutputCache();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
